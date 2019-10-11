1f28de1ce614dfb3dda33dc166bfec74a5d7b531
simon3000 <simon3000@163.com>
2019-10-11 05:26:22 +0200

    demos

---
+++ build.sh
@@ -0,0 +1,9 @@
+cd demo1
+npm install
+npm run build
+cd ..
+
+cd demo2
+npm install
+npm run build
+cd ..
---
+++ demo1/src/App.vue
@@ -0,0 +1,6 @@
+<template>
+  <div id="app">
+    Go to <a href="/demo2">demo2</a>
+    Go to <a href="/demo2/about">demo2/about</a>
+  </div>
+</template>
