14864b4e2fdad8f1415398a5393e489f9986c288
simon3000 <simon3000@163.com>
2019-10-13 10:08:58 +0200

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,10 +1,10 @@
-# rm dist.tar.gz
+rm -r dist
 
-# curl -s https://api.github.com/repos/dd-center/dd.center/releases/latest \
-# | grep "browser_download_url" \
-# | cut -d '"' -f 4 \
-# | wget -qi -
+curl -s https://api.github.com/repos/dd-center/dd.center/releases/latest \
+| grep "browser_download_url" \
+| cut -d '"' -f 4 \
+| wget -qi -
 
-# tar -xzf dist.tar.gz
+tar -xzf dist.tar.gz
 
-
+rm dist.tar.gz
