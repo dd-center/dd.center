8ac7e85ea1e4aa391a485fdb720eb3cdcddc8d0f
kinori <45453624+KinoriN@users.noreply.github.com>
2019-10-14 15:35:31 +0800

    根据文件名获取文件

--- api/update.js
+++ api/update.js
@@ -5,13 +5,22 @@ const update = new Router({
   prefix: '/update'
 })
 
-update.get('/:name/:file', ctx => {
+update.get('/:name/:file', async ctx => {
   const { name, file } = ctx.params
   if (name === 'ddatelectron') {
-    ctx.body = got(`https://github.com/dd-center/DDatElectron/releases/latest/download/${file}`, { stream: true })
+    ctx.body = await got(`https://github.com/dd-center/DDatElectron/releases/latest/download/${file}`, { stream: true })
   }
-  if (name === 'ddmonitor'){
-    ctx.body = got(`https://github.com/dd-center/bili-dd-monitor/releases/latest/download/${file}`,{ stream: true })
+  if (name === 'ddmonitor') {
+    const releases = JSON.parse((await got(`https://api.github.com/repos/dd-center/bili-dd-monitor/releases`)).body);
+    ctx.body = await got(await new Promise((resolve) => {
+      releases.forEach(release => {
+        release.assets.forEach(asset => {
+          if (asset.name === file) {
+            resolve(asset.browser_download_url);
+          }
+        })
+      })
+    }), { stream: true })
   }
 })
 
