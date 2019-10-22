c350432c77e5aef0abab373e146f400b70d8fa7e
simon3000 <simon30002021@icloud.com>
2019-10-23 00:44:58 +0200

    ddcenterhome

--- build.sh
+++ build.sh
@@ -1,17 +1,22 @@
 rm -rf dist
+mkdir dist
+
+cd ddcenterhome
+npm install
+npm run build
+cd ..
+mv ddcenterhome/build/* dist
 
 cd demo1
 npm install
 npm run build
 cd ..
+mv demo1/dist dist/demo1
 
 cd demo2
 npm install
 npm run build
 cd ..
-
-mkdir dist
-mv demo1/dist dist/demo1
 mv demo2/dist dist/demo2
 
 tar -czf dist.tar.gz dist
