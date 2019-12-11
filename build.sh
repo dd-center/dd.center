f6190c914fb2282ee44f4bb8f2cea90fe2063603
simon3000 <simon30002021@icloud.com>
2019-12-11 16:24:12 +0100

    remove demo1, demo2

--- build.sh
+++ build.sh
@@ -10,16 +10,4 @@ npm run build
 cd ..
 mv ddcenterhome/build/* dist
 
-cd demo1
-npm install
-npm run build
-cd ..
-mv demo1/dist dist/demo1
-
-cd demo2
-npm install
-npm run build
-cd ..
-mv demo2/dist dist/demo2
-
 tar -czf dist.tar.gz dist
--- demo1/src/App.vue
+++
@@ -1,9 +0,0 @@
-<template>
-  <div id="app">
-    Go to <a href="/demo2">demo2</a>
-    <br>
-    Go to <a href="/demo2/about">demo2/about</a>
-    <br>
-    <p>Simon到此一游</p>
-  </div>
-</template>
